dd25ecbf707fdf99dfd475efc6c6b4b687235bd8
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2025-10-07 18:01:51 -0500

    Create S1_Exercises.py

---
+++ S1_Exercises.py
@@ -0,0 +1,20 @@
+## E1
+## Realice un pseudocódigo y codigo que permita hallar el área y el volumen de una esfera, si se conocen las siguientes fórmulas:
+## Área = 4 * 3.1415 * r^2
+## Volumen = 4/3 * 3.1415 * r^3
+## Sugerencia: Puedes definir a 3.1415 como una variable llamada 'Pi'
+
+
+
+
+
+## E2 
+## Realice un programa que calcule el indice de masa corporal (IMC), el usuario deberá ingresar su peso y altura, considere la siguiente formula: 
+## IMC = peso / ( altura ** 2 )
+## Sugerencia: Define los inputs como float
+
+
+
+## E3
+## Realice un programa que permita determinar el tiempo en horas y minutos que corrió un atleta. Considerar que el reloj del atleta marca solo minutos
+## Sugerencia: Usar division entera y modulo
