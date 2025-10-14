13458bf801c6277b3089a3a93c361cafa0298e25
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2025-10-14 14:58:58 -0500

    Update S2_Exercises.cpp

--- S2_Exercises.cpp
+++ S2_Exercises.cpp
@@ -12,3 +12,29 @@
 // 2. Enunciado:
 // El usuario ingresa un número n, el programa calcula n! (factorial).
 // Usa bucle for o while.
+
+// input -> 4
+// factorial(4) = 4x3x2x1
+//                1x2x3x4
+
+  // factorial(4) = 4x3x2x1
+  //                1x2x3x4
+  int input1 = 5;
+  int resultado = 1;
+  for(int i = 1; i<=input1; i++){
+    resultado = resultado*i;
+    cout<<"!"<<i<<":"<<resultado<<endl;
+  }
+
+  cout<<endl; // salto de linea
+
+  
+  int k = 1;
+  int resultado2 = 1;
+  int input2 = 6;
+  while(k<=input2){
+    resultado2 = resultado2*k;
+    cout<<"!"<<k<<":"<<resultado2<<endl;
+    k++;
+  }
+
