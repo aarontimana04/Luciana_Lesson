1989b430cc47fba9678b907cf76251a3e61dbad6
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2025-10-14 03:11:55 -0500

    Create S2_Excercises.cpp

---
+++ S2_Excercises.cpp
@@ -0,0 +1,14 @@
+// Mostrar tabla de multiplicar
+// 1. Enunciado:
+// Pide un número y muestra su tabla de multiplicar del 1 al 12.
+// Usa un bucle for.
+
+
+
+
+
+
+// Calcular factorial de un número
+// 2. Enunciado:
+// El usuario ingresa un número n, el programa calcula n! (factorial).
+// Usa bucle for o while.
