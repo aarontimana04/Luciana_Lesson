84488864321b42ceee0880be0860dec8f1e3d80e
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2025-10-15 18:35:52 -0500

    Create S4_Exercises.cpp

---
+++ S4_Exercises.cpp
@@ -0,0 +1,15 @@
+// 1. Crea una función void mostrar_matriz_consecutiva(int filas, int columnas) 
+// que muestre una matriz donde los números van del 1 al filas*columnas, ordenados de izquierda a derecha y de arriba abajo.
+
+
+
+
+
+
+
+// 2. Haz una función void generar_matriz(int filas, int columnas) que cree una matriz con valores aleatorios del 1 al 4 y los muestre.
+
+
+
+
+// 3. Crea una función void tabla_multiplicar(int n) que muestre la tabla de multiplicar del 1 al n (en forma de matriz).
