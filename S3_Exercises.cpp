d8f59f85fdb740790b0637782d484bf7e6e43e44
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2025-10-15 11:22:31 -0500

    Create S3_Exercises.cpp

---
+++ S3_Exercises.cpp
@@ -0,0 +1,10 @@
+// 1. Usar for anidados dentro de una función void para imprimir todas las tablas de multiplicar del 1 al 10.
+
+
+
+
+// 2. Usar for dentro de una función tipo int.
+
+
+
+// 3. Crea un programa en C++ que permita intercambiar los valores de dos variables utilizando punteros.
