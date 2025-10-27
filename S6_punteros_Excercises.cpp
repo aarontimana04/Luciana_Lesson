f8e0c7fe95b69f551569e10b53f27d0466f0c5a8
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2025-10-27 02:44:29 -0500

    Create S6_punteros_Excercises.cpp

---
+++ S6_punteros_Excercises.cpp
@@ -0,0 +1,8 @@
+/*
+Usa memoria dinámica y punteros para crear un programa que:
+- Pida al usuario cuántas notas ingresará.
+- Reserve memoria para ese número de notas.
+- Completa los valores por consola, es decir, pideselo al usuario
+- Calcule el promedio y muestre la nota más alta.
+- Libere la memoria.
+*/
