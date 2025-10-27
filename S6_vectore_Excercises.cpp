9826f30d63d33ecc3fd8a70d4207bca19e0cf6ae
aarontimana04 <112721918+aarontimana04@users.noreply.github.com>
2025-10-27 02:49:02 -0500

    Create S6_vectore_Excercises.cpp

---
+++ S6_vectore_Excercises.cpp
@@ -0,0 +1,9 @@
+/*
+Escribe una función:
+void rotar_derecha(vector<int>& v, int pasos);
+que mueva los elementos del vector hacia la derecha la cantidad de pasos indicada.
+Por ejemplo:
+v = [10, 20, 30, 40]
+rotar_derecha(v, 1) → [40, 10, 20, 30]
+
+*/
